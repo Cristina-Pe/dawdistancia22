7ad46915f376af63c6696516ecfefb6be0120318
Cristina-Pe <88485911+Cristina-Pe@users.noreply.github.com>
2022-05-05 11:05:16 +0200

    Add files via upload

---
+++ practica-perez.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+*Devuelve el nombre introducido
+*
+*@author Cristina Pérez Meneses
+*@version 1.0
+*@param string $nombre parámetro introducido
+*@return string $nombre Devolvemos el nombre
+*@acces private
+*/
+function nombre($nombre){
+echo "Tu nombre es".$nombre;
+return $numero;
+}
+
+/**
+*Devuelve el doble de la eda introducida
+*
+*@author Cristina Pérez Meneses
+*@version 1.1
+*@param string $edad
+*@return string $doble devolvemos un número
+*@acces private
+*/
+function dobleEdad($edad){
+echo "Tu edad es".$edad;
+$doble = $edad*2;
+echo "Y el doble de tu edad es: ".$doble;
+return $doble;
+}
+
+?>
+
